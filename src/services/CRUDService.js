abedf2f870bb769f9856a09780f08db3e0acc33c
luanpc <luanpc2805@gmail.com>
2022-01-11 23:07:48 +0700

    complete CRUD

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -26,7 +26,6 @@ let createNewUser = async (data) => {
 
 }
 
-
 let hashUserPassword = (password) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -37,6 +36,80 @@ let hashUserPassword = (password) => {
         }
     })
 }
+
+
+let getAllUser = () => {
+    // Xu ly bat dong bo dung Promise
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = db.User.findAll({ raw: true });
+            resolve(users); // = return
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+let getUserInfoById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId },
+                raw: true
+            })
+            if (user) {
+                resolve(user)
+            } else {
+                resolve([])
+            }
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+                await user.save();
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            } else {
+                resolve();
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    })
+}
+let deleteUser = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId }
+            })
+
+            if (user) {
+                await user.destroy();
+            }
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+
+    })
+}
 module.exports = {
     createNewUser: createNewUser,
+    getAllUser: getAllUser,
+    getUserInfoById: getUserInfoById,
+    updateUserData: updateUserData,
+    deleteUser: deleteUser
 }
