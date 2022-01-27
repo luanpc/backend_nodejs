72a742b7d7de7cf77c63d6d0dcaf35e30ecd8bf5
luanpc <luanpc2805@gmail.com>
2022-01-27 12:18:26 +0700

    build API get all doctor & save doctor info

--- src/controllers/DoctorController.js
+++ src/controllers/DoctorController.js
@@ -15,6 +15,31 @@ let getTopDoctorHome = async (req, res) => {
     }
 }
 
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+let saveInfoDoctor = async (req, res) => {
+    try {
+        let data = await doctorService.saveDetailInfoDoctor(req.body);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    saveInfoDoctor: saveInfoDoctor
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -27,6 +27,53 @@ let getTopDoctorHome = (limit) => {
     })
 }
 
+let getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where: { roleId: 'R2' },
+                attributes: {
+                    exclude: ['password', 'image']
+                },
+            });
+            resolve({
+                errCode: 0,
+                data: doctors
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let saveDetailInfoDoctor = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.doctorId || !data.contentHTML || !data.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            } else {
+                await db.Markdown.create({
+                    contentHTML: data.contentHTML,
+                    contentMarkdown: data.contentMarkdown,
+                    description: data.description,
+                    doctorId: data.doctorId
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: 'Save info doctor success'
+                })
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    saveDetailInfoDoctor: saveDetailInfoDoctor
 }
