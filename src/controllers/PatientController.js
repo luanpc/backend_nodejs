51bdcda7fb3978c052360ba7cc12c7bf724a44f6
luanpc <luanpc2805@gmail.com>
2022-02-06 10:47:27 +0700

    patient verify booking appointment

--- src/controllers/PatientController.js
+++ src/controllers/PatientController.js
@@ -11,6 +11,18 @@ let postBookAppointment = async (req, res) => {
     }
 }
 
+let postVerifyBookAppointment = async (req, res) => {
+    try {
+        let info = await patientService.postVerifyBookAppointment(req.body);
+        return res.status(200).json(info);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
-    postBookAppointment
+    postBookAppointment,
+    postVerifyBookAppointment
 }
