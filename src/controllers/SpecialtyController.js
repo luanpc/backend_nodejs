504152bebde92525b559195dba033983f944e981
luanpc <luanpc2805@gmail.com>
2022-02-07 08:50:52 +0700

    build API get all specialties

--- src/controllers/SpecialtyController.js
+++ src/controllers/SpecialtyController.js
@@ -11,6 +11,18 @@ let createSpecialty = async (req, res) => {
         })
     }
 }
+let getAllSpecialty = async (req, res) => {
+    try {
+        let info = await specialtyService.getAllSpecialty();
+        return res.status(200).json(info);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 module.exports = {
-    createSpecialty
+    createSpecialty,
+    getAllSpecialty
 }
